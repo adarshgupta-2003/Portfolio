dc02e67bb2bb4a4b5b7c750b9774bd10764e9fee
adarshgupta-2003 <adarshguptaa.dev@gmail.com>
2024-02-15 04:36:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function showSideBar() {
+    const mobileBar = document.querySelector('.mobile-nav');
+    const allContent = document.querySelector('.container-main');
+    allContent.style.display = 'none';
+    mobileBar.style.display="block"
+}
+
+function closeSideBar() {
+    const allContent = document.querySelector('.container-main');
+    allContent.style.display = 'block';
+    const mobileBar = document.querySelector('.mobile-nav');
+    mobileBar.style.display="none"
+}
