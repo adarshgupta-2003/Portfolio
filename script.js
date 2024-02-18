f423fa9d1d9aa371d4560837358d17ee7d5f87f1
adarshgupta-2003 <adarshguptaa.dev@gmail.com>
2024-02-18 20:11:29 +0530

    responsive hero-section:issue

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function showSideBar() {
-    const mobileBar = document.querySelector('.mobile-nav');
-    const allContent = document.querySelector('.container-main');
+    const mobileBar = document.querySelector('.mob-nav-popup');
+    const allContent = document.querySelector('.main-container');
     allContent.style.display = 'none';
     mobileBar.style.display="block"
 }
 
 function closeSideBar() {
-    const allContent = document.querySelector('.container-main');
+    const allContent = document.querySelector('.main-container');
     allContent.style.display = 'block';
-    const mobileBar = document.querySelector('.mobile-nav');
+    const mobileBar = document.querySelector('.mob-nav-popup');
     mobileBar.style.display="none"
 }
+
+// Typing Animation using Typed JS
+var typed = new Tayped(".type", {
+    strings: ["Web Developement", "Machine Learning", "and Data Science"],
+    smartBackspace: true,
+    startDelay: 1000,
+    typeSpeed: 50,
+    backDelay: 1000,
+    backSpeed: 60,
+    loop: true,
+  });
+  
