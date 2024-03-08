222ecce00701536fd6e10f392024d3ad5e3725cc
adarshgupta-2003 <adarshguptaa.dev@gmail.com>
2024-03-09 03:10:24 +0530

    feat: about-section

--- script.js
+++ script.js
@@ -12,6 +12,42 @@ function closeSideBar() {
     mobileBar.style.display="none"
 }
 
+function qualClick(){
+    const qualBody = document.querySelector('.qual-body');
+    const qualText = document.querySelector('.qual');
+    const expText = document.querySelector('.exp');
+    const expBody = document.querySelector('.exp-body');
+    const qualIcon = document.querySelector('.qualcolor');
+    const expIcon = document.querySelector('.expcolor');
+
+
+    qualIcon.style.color= '#6D57E0';
+    expIcon.style.color= 'white';
+    qualText.style.color= '#6D57E0';
+    expText.style.color= 'white';
+
+    expBody.style.display= 'none';
+    qualBody.style.display= 'block';
+
+}
+
+function expClick(){
+    const qualBody = document.querySelector('.qual-body');
+    const expBody = document.querySelector('.exp-body');
+    const qualText = document.querySelector('.qual');
+    const qualIcon = document.querySelector('.qualcolor');
+    const expText = document.querySelector('.exp');
+    const expIcon = document.querySelector('.expcolor');
+
+    qualBody.style.display= 'none';
+    expBody.style.display= 'block';
+
+    qualIcon.style.color= 'white';
+    expIcon.style.color= '#6D57E0';
+    qualText.style.color= 'white';
+    expText.style.color= '#6D57E0';
+}
+
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
     strings: ["Web Developement", "Machine Learning", "Data Science"],
