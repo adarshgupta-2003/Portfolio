73142d426b8c7075adc7f7eb78da6656864bc588
adarshgupta-2003 <adarshgupta.contact@gmail.com>
2024-03-29 16:22:14 +0530

    feat: creative-page

---
+++ assets/pages/crscript.js
@@ -0,0 +1,39 @@
+var blogs = document.querySelector('.blogs');
+var poems = document.querySelector('.poems');
+var quotes = document.querySelector('.quotes');
+
+var blogsContent = document.querySelector('.blogsContent');
+var poemsContent = document.querySelector('.poemsContent');
+var quotesContent = document.querySelector('.quotesContent');
+
+
+function poemClick(){
+    poems.style.color= '#6D57E0';
+    blogs.style.color= 'white';
+    quotes.style.color= 'white';
+
+    blogsContent.style.display= 'none';
+    poemsContent.style.display= 'flex';
+    quotesContent.style.display= 'none';
+}
+
+function blogClick(){
+    poems.style.color= 'white';
+    blogs.style.color= '#6D57E0';
+    quotes.style.color= 'white';
+
+    blogsContent.style.display= 'flex';
+    poemsContent.style.display= 'none';
+    quotesContent.style.display= 'none';
+}
+
+function quoteClick(){
+    poems.style.color= 'white';
+    blogs.style.color= 'white';
+    quotes.style.color= '#6D57E0';
+
+    blogsContent.style.display= 'none';
+    poemsContent.style.display= 'none';
+    quotesContent.style.display= 'flex';
+}
+
